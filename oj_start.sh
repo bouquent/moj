635d2669e9ba6347f3157791b0e87a972c73fc7b
bouquent <1870058972@qq.com>
2023-02-20 15:43:33 +0800

    fix bug of init uoj data source

--- oj_start.sh
+++ oj_start.sh
@@ -2,3 +2,5 @@ service postgresql start
 python3 /opt/uoj/judger/judge_client start
 cd /opt/server && forever /opt/server/bin/www start &
 
+# update uoj data source
+cd /opt/server/data && node update-problemset.js
